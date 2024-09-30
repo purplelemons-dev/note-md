0a3fbdf122d7a1a00619af22d72d8a7a6aa93163
purplelemons-dev <me@joshsmith.dev>
2024-09-29 22:50:45 -0500

    basic functionality--the best kind

---
+++ src/routes/+page.svelte
@@ -0,0 +1,41 @@
+<script lang="ts">
+	import { marked } from 'marked';
+	let editorContent = '';
+	$: preview = marked(editorContent);
+</script>
+
+<main>
+	<div class="editor">
+		<textarea bind:value={editorContent} />
+	</div>
+	<div class="preview">
+		{@html preview}
+	</div>
+</main>
+
+<style>
+	main {
+		width: 97vw;
+		height: 97vh;
+		display: flex;
+		margin: 1%;
+	}
+
+	.editor,
+	.preview {
+		width: 50%;
+		height: 100%;
+		background-color: #f0f0f0;
+	}
+
+	.preview {
+		padding-left: 1%;
+	}
+
+	textarea {
+		width: 100%;
+		height: 100%;
+		border: none;
+		resize: none;
+	}
+</style>
